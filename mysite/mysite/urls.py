10edd7b02fbd34ae814e1770fe0e30a5d998b686
DLCQQ <vlad.koles100@gmail.com>
2024-02-08 22:55:23 +0300

    i_need_help2

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -2,9 +2,8 @@ from django.urls import path
 from myapp import views
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
-    # path('', views.clients, name='clients'),
+    path('', views.home, name='home'),
+    path('clients/', views.clients, name='clients'),
     path('products/', views.products, name='products'),
     path('orders/', views.orders, name='orders'),
-    path('save_client_data/', views.save_client_data, name='save_client_data')
 ]
